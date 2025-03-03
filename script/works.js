e967320b366b30c6a70c681014642c7bff0f1809
The Ngoc <np1552005@gmail.com>
2025-03-03 17:16:08 +0700

    init files, finish work

---
+++ script/works.js
@@ -0,0 +1,7 @@
+const changeDescription = (text) => {
+  document.querySelector(".description").innerHTML = text;
+}
+
+const resetDescription = () => {
+  document.querySelector(".description").innerHTML = "Hover over these image!";
+}
